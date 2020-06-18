dfeff9a1b841e0845b2b9d89651aa8d50ddc6b48
maha gazalen <maha.gazalen.97@gmail.com>
2020-06-18 18:28:02 +0300

    contact us design

--- Client/src/app/contact-us/contact-us.component.ts
+++ Client/src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BackendService } from '../services/backend.service';
 
 @Component({
   selector: 'app-contact-us',
@@ -7,7 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+  error: boolean = false;
+  errorMessage: String = "";
+  dataLoading: boolean = false;
+  private querySubscription;
+  savedChanges: boolean = false;
+  docData;
+
+  constructor(private _backendService: BackendService, private _route: Router) { }
+
+  settings(formData){
+    this.dataLoading = true;
+    this.error = false;
+          this.errorMessage = "";
+          this.dataLoading = false;
+          this.savedChanges = true;
+
+
+
+  //   this.querySubscription = this._backendService.updateUser(formData).subscribe((res) => {
+  //     if (res["errorCode"] > 0) {
+  //         this.error = false;
+  //         this.errorMessage = "";
+  //         this.dataLoading = false;
+  //         this.savedChanges = true;
+  //     } else {
+  //         this.error = true;
+  //         this.errorMessage = res["errorMessage"];
+  //         this.dataLoading = false;
+  //     }
+  // },
+  //     (error) => {
+  //         this.error = true;
+  //         this.errorMessage = error.message;
+  //         this.dataLoading = false;
+  //     },
+  //     () => {
+  //         this.dataLoading = false;
+  //     });
+}
+
 
   ngOnInit(): void {
   }
